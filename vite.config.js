520f1a6a3ac37737a949065c95922785dcf8af12
Jason Murphy <jasonbm76@gmail.com>
2024-04-09 14:36:56 -0500

    Add assets

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "http://jasonmurphy.tech/",
-	plugins: [react()],
-});
+  plugins: [react()],
+})
