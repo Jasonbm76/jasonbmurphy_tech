533e53481b00425b636d7e88505d0aab5b7f759e
Jason Murphy <jasonbm76@gmail.com>
2024-04-19 14:33:33 -0500

    Make Projects more dynamic

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -8,6 +8,48 @@ import { Stack } from '@mui/material';
 
 import './projects.scss';
 
+const projectsArray = [
+  {
+    name: 'Bluehost Homepage',
+    image: '/bluehost.jpg',
+    alt: 'Bluehost Screenshot',
+    description:
+      'Bluehost is one of many brands owned by Newfold Digital. This is the front end of the Bluehost website that I worked on. I also spent considerable time working on the customer portal and signup flow for the website.',
+  },
+  {
+    name: 'BlueHost Plan Selection',
+    image: '/bluehost2.jpg',
+    alt: 'BlueHost Screenshot',
+    description:
+      'Throughout the website there are many challenging areas where we have to fit a dynamic number of plan tiers with dynamic text into the flow of the website and have it look great on all devices.',
+  },
+  {
+    name: 'HostGator Homepage',
+    image: '/hostgator.jpg',
+    alt: 'HostGator Screenshot',
+    description:
+      'Throughout the website there are many challenging areas where we have to fit a dynamic number of plan tiers with dynamic text into the flow of the website and have it look great on all devices.',
+  },
+];
+
+const Project = ({ name, image, alt, description }) => {
+  return (
+    <Card sx={{ border: 0 }}>
+      <CardActionArea>
+        <CardMedia component="img" image={image} alt={alt} />
+        <CardContent>
+          <Typography variant="h5" component="div">
+            {name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
 const Projects = () => {
   return (
     <div>
@@ -19,55 +61,16 @@ const Projects = () => {
       </Typography>
       <Divider className="horizontal-divider" />
       <Stack spacing={2} className="projects">
-        <Card sx={{ border: 0 }}>
-          <CardActionArea>
-            <CardMedia component="img" image="/bluehost.jpg" alt="Bluehost Screenshot" />
-            <CardContent>
-              <Typography variant="h5" component="div">
-                Bluehost Homepage
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Bluehost is one of many brands owned by Newfold Digital. This is the front end of the Bluehost website that I worked on. I also spent
-                considerable time working on the customer portal and signup flow for the website.
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-        <Card sx={{ border: 0 }}>
-          <CardActionArea>
-            <CardMedia component="img" image="/bluehost2.jpg" alt="BlueHost Screenshot" />
-            <CardContent>
-              <Typography variant="h5" component="div">
-                BlueHost Plan Selection
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Throughout the website there are many challenging areas where we have to fit a dynamic number of plan tiers with dynamic text into the flow of
-                the website and have it look great on all devices.
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-        <Card sx={{ border: 0 }}>
-          <CardActionArea>
-            <CardMedia component="img" image="/hostgator.jpg" alt="HostGator Screenshot" />
-            <CardContent>
-              <Typography variant="h5" component="div">
-                HostGator Homepage
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                The other main brand that I worked on was Hostgator. Hostgator was more challenging as it is known in the industry as more of a fun and playful
-                brand and we wanted to keep that up while also making it look more modern and professional.
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
+        {projectsArray.map((link, index) => (
+          <Project key={index} {...link} />
+        ))}
         <Typography variant="h5" component="h5">
           Note
         </Typography>
         <Typography variant="body1" component="p" className="related">
-          These are just a few of the projects that I have worked on. I have also worked on many other projects that are not listed here. Because I have spent
-          my entire career working on projects for other companies, I do not have permission to show the source code. I have worked on everything from
-          refactoring legacy monolithic applications to building new applications from scratch.
+          These are just a few of the projects that I have worked on, although I have many other projects that are not listed here. Since I have spent my entire
+          career working on projects for other companies, I do not have permission to show the source code. I have worked on everything from refactoring legacy
+          monolithic applications to building new applications from scratch.
         </Typography>
       </Stack>
     </div>
