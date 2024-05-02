318ff6de6ca9c5e18d05d7f41c100599695bc875
Jason Murphy <jasonbm76@gmail.com>
2024-05-02 14:56:03 -0500

    Update header to slide out of view when scrolling down and scroll back up when in view.
    Add 3 more projects to my gallery section.
    
    Make project gallery images clickable to view the actual website

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,97 +1,169 @@
 import './header.scss';
 import { motion } from 'framer-motion';
-import Box from '@mui/material/Box';
+//import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
+import { Box, HStack } from '@chakra-ui/react';
 import React from 'react';
 import { Sling as Hamburger } from 'hamburger-react';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import Divider from '@mui/material/Divider';
 
 const Links = ['about', 'resume', 'projects', 'skills', 'contact'];
 
 const Spacer = () => {
-  return <Box component="span" sx={{ mx: 1 }} />;
+	return (
+		<span
+			component='span'
+			sx={{ mx: 1 }}
+		/>
+	);
 };
 
 const Header = () => {
-  const [isOpen, setOpen] = useState(false);
+	const [isOpen, setOpen] = useState(false);
+	const headerRef = useRef(null);
 
-  const handleClick = () => {
-    setOpen(false);
-  };
+	useEffect(() => {
+		let prevScrollPos = window.scrollY;
 
-  const LinkItem = ({ href, title }) => {
-    return (
-      <motion.li
-        whileHover={{
-          scale: 1,
-          y: -5,
-          transition: { duration: 0.5 },
-        }}
-        whileTap={{ scale: 0.9 }}
-      >
-        <a href={href} title={title} onClick={() => handleClick()}>
-          {title}
-        </a>
-      </motion.li>
-    );
-  };
+		const handleScroll = () => {
+			const currScrollPos = window.scrollY;
+			const currHeaderElement = headerRef.current;
 
-  return (
-    <header>
-      <div className="logoContainer">
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5, delay: 0.5 }}>
-          <h1>
-            <a href="#" title="JASON MURPHY">
-              Jason Murphy
-            </a>
-          </h1>
-        </motion.div>
-      </div>
+			if (!currHeaderElement) return;
 
-      <div className="topNav">
-        <div className="hamburgerMenu">
-          <Hamburger toggled={isOpen} size={16} toggle={setOpen} label="Show Menu" />
+			prevScrollPos > currScrollPos
+				? (currHeaderElement.style.transform = 'translateY(0)')
+				: (currHeaderElement.style.transform = 'translateY(-200px)');
 
-          <div className="mobileNavMenu" style={{ display: isOpen ? 'block' : 'none' }}>
-            <ul>
-              <LinkItem href="#" title="Home" />
-              <Divider component="li" />
-              {Links.map((link, index) => (
-                <React.Fragment key={`spacer-${index}`}>
-                  <LinkItem href={`#${link}`} title={link.toUpperCase()} key={index} />
-                  <Divider component="li" />
-                </React.Fragment>
-              ))}
-            </ul>
-          </div>
-        </div>
+			prevScrollPos = currScrollPos;
+		};
 
-        <motion.ul className="desktopNavMenu" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5, delay: 0.5 }}>
-          <motion.li
-            whileHover={{
-              scale: 1,
-              y: -5,
-              transition: { duration: 0.5 },
-            }}
-            whileTap={{ scale: 0.9 }}
-          >
-            <Link href="#" title="Home" className="homeIcon">
-              <HomeOutlinedIcon />
-            </Link>
-          </motion.li>
-          <Spacer />
-          {Links.map((link, index) => (
-            <React.Fragment key={`spacer-${index}`}>
-              <LinkItem href={`#${link}`} title={link.toUpperCase()} key={index} />
-              <Spacer key={`spacer-${index}`} />
-            </React.Fragment>
-          ))}
-        </motion.ul>
-      </div>
-    </header>
-  );
+		window.addEventListener('scroll', handleScroll);
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
+
+	const handleClick = () => {
+		setOpen(false);
+	};
+
+	const LinkItem = ({ href, title }) => {
+		return (
+			<motion.li
+				whileHover={{
+					scale: 1,
+					y: -5,
+					transition: { duration: 0.5 },
+				}}
+				whileTap={{ scale: 0.9 }}>
+				<a
+					href={href}
+					title={title}
+					onClick={() => handleClick()}>
+					{title}
+				</a>
+			</motion.li>
+		);
+	};
+
+	return (
+		<Box
+			position='fixed'
+			zIndex='2000'
+			top={0}
+			left={0}
+			right={0}
+			translateY={0}
+			transitionProperty='transform'
+			transitionDuration='.3s'
+			transitionTimingFunction='ease-in-out'
+			ref={headerRef}>
+			<header>
+				<div className='logoContainer'>
+					<motion.div
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						transition={{ duration: 0.5, delay: 0.5 }}>
+						<h1>
+							<a
+								href='#'
+								title='JASON MURPHY'>
+								Jason Murphy
+							</a>
+						</h1>
+					</motion.div>
+				</div>
+				<div className='topNav'>
+					<div className='hamburgerMenu'>
+						<Hamburger
+							toggled={isOpen}
+							size={16}
+							toggle={setOpen}
+							label='Show Menu'
+						/>
+
+						<div
+							className='mobileNavMenu'
+							style={{ display: isOpen ? 'block' : 'none' }}>
+							<ul>
+								<LinkItem
+									href='#'
+									title='Home'
+								/>
+								<Divider component='li' />
+								{Links.map((link, index) => (
+									<React.Fragment key={`spacer-${index}`}>
+										<LinkItem
+											href={`#${link}`}
+											title={link.toUpperCase()}
+											key={index}
+										/>
+										<Divider component='li' />
+									</React.Fragment>
+								))}
+							</ul>
+						</div>
+					</div>
+
+					<motion.ul
+						className='desktopNavMenu'
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						transition={{ duration: 0.5, delay: 0.5 }}>
+						<motion.li
+							whileHover={{
+								scale: 1,
+								y: -5,
+								transition: { duration: 0.5 },
+							}}
+							whileTap={{ scale: 0.9 }}>
+							<Link
+								href='#'
+								title='Home'
+								className='homeIcon'>
+								<HomeOutlinedIcon />
+							</Link>
+						</motion.li>
+						<Spacer />
+						{Links.map((link, index) => (
+							<React.Fragment key={`spacer-${index}`}>
+								<LinkItem
+									href={`#${link}`}
+									title={link.toUpperCase()}
+									key={index}
+								/>
+								<Spacer key={`spacer-${index}`} />
+							</React.Fragment>
+						))}
+					</motion.ul>
+				</div>
+			</header>
+		</Box>
+	);
 };
 
 export default Header;
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -4,6 +4,7 @@ import Typography from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
+import Link from '@mui/material/Link';
 import { CardActionArea } from '@mui/material';
 import { Stack } from '@mui/material';
 import Slider from 'react-slick';
@@ -14,103 +15,182 @@ import 'slick-carousel/slick/slick-theme.css';
 import './projects.scss';
 
 const projectsArray = [
-  {
-    id: 1,
-    name: 'Bluehost Homepage',
-    image: '/bluehost.jpg',
-    alt: 'Bluehost Screenshot',
-    description:
-      'Bluehost is one of many brands owned by Newfold Digital. This is the front end of the Bluehost website that I worked on. I also spent considerable time working on the customer portal and signup flow for the website.',
-  },
-  {
-    id: 2,
-    name: 'BlueHost Plan Selection',
-    image: '/bluehost2.jpg',
-    alt: 'BlueHost Screenshot',
-    description:
-      'Throughout the website there are many challenging areas where we have to fit a dynamic number of plan tiers with dynamic text into the flow of the website and have it look great on all devices.',
-  },
-  {
-    id: 3,
-    name: 'HostGator Homepage',
-    image: '/hostgator.jpg',
-    alt: 'HostGator Screenshot',
-    description:
-      'Throughout the website there are many challenging areas where we have to fit a dynamic number of plan tiers with dynamic text into the flow of the website and have it look great on all devices.',
-  },
+	{
+		id: 1,
+		link: 'https://www.hostgator.com/',
+		name: 'HostGator Homepage',
+		image: '/hostgator.jpg',
+		alt: 'HostGator Screenshot',
+		description:
+			'Throughout the website there are many challenging areas where we have to fit a dynamic number of plan tiers with dynamic text into the flow of the website and have it look great on all devices.',
+	},
+	{
+		id: 2,
+		link: 'https://www.bluehost.com/',
+		name: 'Bluehost Homepage',
+		image: '/bluehost.jpg',
+		alt: 'Bluehost Screenshot',
+		description:
+			'Bluehost is one of many brands owned by Newfold Digital. This is the front end of the Bluehost website that I worked on. I also spent considerable time working on the customer portal and signup flow for the website.',
+	},
+	{
+		id: 3,
+		link: 'https://www.bluehost.com/hosting/shared#pricing-cards',
+		name: 'BlueHost Plan Selection',
+		image: '/bluehost2.jpg',
+		alt: 'BlueHost Screenshot',
+		description:
+			'Throughout the website there are many challenging areas where we have to fit a dynamic number of plan tiers with dynamic text into the flow of the website and have it look great on all devices.',
+	},
+	{
+		id: 4,
+		link: 'https://www.web.com/',
+		name: 'Web.com Homepage',
+		image: '/web_com.jpg',
+		alt: 'Web.com Screenshot',
+		description:
+			'The challenge here was making a website that was dynamic and could be used for multiple brands. The website is used for Web.com, Domain.com, Network Solutions, and Register.com.',
+	},
+	{
+		id: 5,
+		link: 'https://www.web.com/',
+		name: 'Web.com Project Gallery',
+		image: '/web_com_gallery.jpg',
+		alt: 'Web.com Screenshot',
+		description:
+			'Project gallery showing a small sample of many customer websites that we built. The gallery is dynamic and can be updated by the marketing team without needing a developer to make changes.',
+	},
+	{
+		id: 6,
+		link: 'https://www.web.com/websites/templates',
+		name: 'Web.com Client Template Gallery',
+		image: '/web_com_templates.jpg',
+		alt: 'Web.com Screenshot',
+		description:
+			'A large selection of web templates which customers can use to create their new website. The templates are responsive and look great on all devices.',
+	},
 ];
 
-const Project = ({ name, image, alt, description }) => {
-  return (
-    <Card sx={{ border: 0 }}>
-      <CardActionArea>
-        <CardMedia component="img" image={image} alt={alt} />
-        <CardContent>
-          <Typography variant="h5" component="div">
-            {name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-  );
+const MobileProject = ({ name, link, image, alt, description }) => {
+	return (
+		<Card
+			sx={{ border: 0 }}
+			className='mobileProjectContainer'>
+			<CardActionArea>
+				<Link
+					href={link}
+					target='_blank'
+					rel='noreferrer'
+					title={`Click to open the ${name} project in a new tab`}>
+					<CardMedia
+						component='img'
+						image={image}
+						alt={alt}
+					/>
+				</Link>
+				<CardContent>
+					<Typography
+						variant='h5'
+						component='div'>
+						{name}
+					</Typography>
+					<Typography
+						variant='body2'
+						color='text.secondary'>
+						{description}
+					</Typography>
+				</CardContent>
+			</CardActionArea>
+		</Card>
+	);
 };
 
 const Projects = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
+	const settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+	};
 
-  return (
-    <div>
-      <Typography variant="h2" component="h2">
-        My Projects
-      </Typography>
-      <Typography variant="body1" component="p" className="lead">
-        These are some of the things that I have worked on in the recent past.
-      </Typography>
-      <Divider className="horizontal-divider" />
+	return (
+		<div>
+			<Typography
+				variant='h2'
+				component='h2'>
+				My Projects
+			</Typography>
+			<Typography
+				variant='body1'
+				component='p'
+				className='lead'>
+				These are some of the things that I have worked on in the recent past.
+			</Typography>
+			<Divider className='horizontal-divider' />
 
-      <Stack spacing={2} className="projects">
-        <div className="sliderContainer">
-          <Slider {...settings}>
-            {projectsArray.map((item) => (
-              <div key={item.id}>
-                <img src={item.image} alt={item.alt} className="sliderImg" />
-                <Typography variant="h5" component="h5">
-                  {item.name}
-                </Typography>
-                <Typography variant="body2" color="text.secondary" className="description">
-                  {item.description}
-                </Typography>
-              </div>
-            ))}
-          </Slider>
-        </div>
+			<Stack
+				spacing={2}
+				className='projects'>
+				<div className='sliderContainer'>
+					<Slider {...settings}>
+						{projectsArray.map((item) => (
+							<div key={item.id}>
+								<a
+									href={item.link}
+									title={`Click to open the ${item.name} project in a new tab`}
+									rel='noreferrer'
+									target='_blank'>
+									<img
+										src={item.image}
+										alt={item.alt}
+										className='sliderImg'
+									/>
+								</a>
+								<Typography
+									variant='h5'
+									component='h5'>
+									{item.name}
+								</Typography>
+								<Typography
+									variant='body2'
+									color='text.secondary'
+									className='description'>
+									{item.description}
+								</Typography>
+							</div>
+						))}
+					</Slider>
+				</div>
 
-        <div className="mobileGallery">
-          {projectsArray.map((link, index) => (
-            <Project key={index} {...link} />
-          ))}
-        </div>
+				<div className='mobileGallery'>
+					{projectsArray.map((link, index) => (
+						<MobileProject
+							key={index}
+							{...link}
+						/>
+					))}
+				</div>
 
-        <Typography variant="h5" component="h5">
-          Note
-        </Typography>
-        <Typography variant="body1" component="p" className="related">
-          These are just a few of the projects that I have worked on, although I have many other projects that are not listed here. Since I have spent my entire
-          career working on projects for other companies, I do not have permission to show the source code. I have worked on everything from refactoring legacy
-          monolithic applications to building new applications from scratch.
-        </Typography>
-      </Stack>
-    </div>
-  );
+				<Typography
+					variant='h5'
+					component='h5'>
+					Note
+				</Typography>
+				<Typography
+					variant='body1'
+					component='p'
+					className='related'>
+					These are just a few of the projects that I have worked on, although I
+					have many other projects that are not listed here. Since I have spent
+					my entire career working on projects for other companies, I do not
+					have permission to show the source code. I have worked on everything
+					from refactoring legacy monolithic applications to building new
+					applications from scratch.
+				</Typography>
+			</Stack>
+		</div>
+	);
 };
 
 export default Projects;
